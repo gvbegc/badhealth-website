d50f0fefaadb78fb51ca0768195b12f293b80a9c
gvbegc <Gabrielggsolis@gmail.com>
2026-02-26 16:23:30 -0800

    Remove diet cards section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from "./components/sections/HeroSection";
 import FeaturesSection from "./components/sections/FeaturesSection";
-import DietCardsSection from "./components/sections/DietCardsSection";
 import AboutSection from "./components/sections/AboutSection";
 import CTASection from "./components/sections/CTASection";
 
@@ -9,7 +8,6 @@ export default function Home() {
     <main>
       <HeroSection />
       <FeaturesSection />
-      <DietCardsSection />
       <AboutSection />
       <CTASection />
     </main>
